a83103c3980ecae7fd990a785c5cf041484ca7a8
Ruin0x11 <ipickering2@gmail.com>
2022-01-03 20:48:54 -0800

    Stat serializers
    Since stats are meant to be transient, I'll omit the buffed value for them by default when saving.

---
+++ OpenNefia.Tests/Core/Stats/Serialization/StatSerializerPartialTest.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using OpenNefia.Core.Serialization.Manager;
+using OpenNefia.Core.Serialization.Markdown.Mapping;
+using OpenNefia.Core.Serialization.Markdown.Value;
+using OpenNefia.Core.Stats;
+using OpenNefia.Core.Stats.Serialization;
+using OpenNefia.Tests.Core.Serialization;
+
+// ReSharper disable AccessToStaticMemberViaDerivedType
+
+namespace OpenNefia.Tests.Core.Stats.Serialization
+{
+    [TestFixture]
+    [TestOf(typeof(StatSerializerPartial<>))]
+    public class StatSerializerPartialTest : SerializationTest
+    {
+        /// <summary>
+        /// The default <see cref="Stat{T}"/> serializer will only save the 
+        /// base value, to reduce the size of the save. (Stats are meant to 
+        /// be transient and recalculable at any time.)
+        /// </summary>
+        [Test]
+        public void SerializationTest_Defaults()
+        {
+            var stat = new Stat<int>(1, 42);
+            var node = Serialization.WriteValueAs<ValueDataNode>(stat);
+
+            Assert.That(node.Value, Is.EqualTo("1"));
+        }
+
+        [Test]
+        public void DeserializationTest_Value()
+        {
+            var stat = new Stat<int>(1, 1);
+            var node = new ValueDataNode("1");
+
+            var deserializedStat = Serialization.ReadValue<Stat<int>>(node);
+
+            Assert.That(deserializedStat, Is.EqualTo(stat));
+        }
+
+        [Test]
+        public void DeserializationTest_Mapping()
+        {
+            var stat = new Stat<int>(1, 42);
+            var node = new MappingDataNode();
+
+            node.Add("base", new ValueDataNode("1"));
+            node.Add("buffed", new ValueDataNode("42"));
+
+            var deserializedStat = Serialization.ReadValue<Stat<int>>(node);
+
+            Assert.That(deserializedStat, Is.EqualTo(stat));
+        }
+    }
+}
