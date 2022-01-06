db0f98329a30f2067e79bb8d5cbed19ec0260372
Ruin0x11 <ipickering2@gmail.com>
2022-01-05 17:05:27 -0800

    Map transfer system test

---
+++ OpenNefia.Content.Tests/Maps/Entrances/MapTransferSystem_Tests.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+using OpenNefia.Content.Maps;
+using OpenNefia.Core.Game;
+using OpenNefia.Core.GameObjects;
+using OpenNefia.Core.Maps;
+using OpenNefia.Core.Maths;
+using OpenNefia.Core.SaveGames;
+using OpenNefia.Tests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenNefia.Content.Tests.Maps.Entrances
+{
+    [TestFixture]
+    [TestOf(typeof(MapTransferSystem))]
+    public class MapTransferSystem_Tests
+    {
+        [Test]
+        public void TestMapTransferForPlayer()
+        {
+            var sim = ContentFullGameSimulation
+                .NewSimulation()
+                .InitializeInstance();
+
+            var entMan = sim.Resolve<IEntityManager>();
+            var mapMan = sim.Resolve<IMapManager>();
+            var gameSess = sim.Resolve<IGameSessionManager>();
+            var saveGameMan = sim.Resolve<ISaveGameManager>();
+            var mapLoader = sim.Resolve<IMapLoader>();
+
+            var map1 = sim.CreateMapAndSetActive(10, 10);
+            var map2 = mapMan.CreateMap(10, 10);
+
+            using var save = new TempSaveGameHandle();
+            saveGameMan.CurrentSave = save;
+
+            var player = entMan.SpawnEntity(null, map1.AtPos(Vector2i.One));
+            gameSess.Player = player;
+            entMan.EnsureComponent<PlayerComponent>(player.Uid);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(mapLoader.MapExistsInSave(map1.Id, save), Is.False);
+                Assert.That(mapLoader.MapExistsInSave(map2.Id, save), Is.False);
+            });
+
+            var expectedPos = new Vector2i(3, 4);
+            player.Spatial.Coordinates = map2.AtPosEntity(expectedPos);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(mapLoader.MapExistsInSave(map1.Id, save), Is.True, "Map 1 exists");
+                Assert.That(mapLoader.MapExistsInSave(map2.Id, save), Is.False, "Map 2 exists");
+                Assert.That(mapMan.MapIsLoaded(map1.Id), Is.False, "Map 1 is loaded");
+                Assert.That(mapMan.MapIsLoaded(map2.Id), Is.True, "Map 2 is loaded");
+                Assert.That(mapMan.ActiveMap?.Id, Is.EqualTo(map2.Id), "Map 2 is active");
+                Assert.That(player.Spatial.MapPosition, Is.EqualTo(map2.AtPos(expectedPos)), "Position was updated");
+            });
+        }
+    }
+}
