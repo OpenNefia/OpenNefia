2c193789842b67b00285f2369106d39134f2d0aa
Ruin0x11 <ipickering2@gmail.com>
2022-01-06 12:01:32 -0800

    Add body parts

---
+++ OpenNefia.Content/Prototypes/Protos/Protos.BodyPart.cs
@@ -0,0 +1,22 @@
+﻿using BodyPartPrototypeId = OpenNefia.Core.Prototypes.PrototypeId<OpenNefia.Content.Equipment.BodyPartPrototype>;
+
+namespace OpenNefia.Content.Prototypes
+{
+    public static partial class Protos
+    {
+        public static class BodyPart
+        {
+            public static readonly BodyPartPrototypeId Head = new($"Elona.{nameof(Head)}");
+            public static readonly BodyPartPrototypeId Neck = new($"Elona.{nameof(Neck)}");
+            public static readonly BodyPartPrototypeId Back = new($"Elona.{nameof(Back)}");
+            public static readonly BodyPartPrototypeId Body = new($"Elona.{nameof(Body)}");
+            public static readonly BodyPartPrototypeId Hand = new($"Elona.{nameof(Hand)}");
+            public static readonly BodyPartPrototypeId Ring = new($"Elona.{nameof(Ring)}");
+            public static readonly BodyPartPrototypeId Arm = new($"Elona.{nameof(Arm)}");
+            public static readonly BodyPartPrototypeId Waist = new($"Elona.{nameof(Waist)}");
+            public static readonly BodyPartPrototypeId Leg = new($"Elona.{nameof(Leg)}");
+            public static readonly BodyPartPrototypeId Ranged = new($"Elona.{nameof(Ranged)}");
+            public static readonly BodyPartPrototypeId Ammo = new($"Elona.{nameof(Ammo)}");
+        }
+    }
+}
---
+++ OpenNefia.Content/Resources/Locale/ja_JP/Elona/Prototypes/BodyPart.lua
@@ -0,0 +1,36 @@
+OpenNefia.Prototypes.Elona.BodyPart.Elona =
+{
+    Head = {
+       Name = "頭"
+    },
+    Neck = {
+       Name = "首"
+    },
+    Back = {
+       Name = "背中"
+    },
+    Body = {
+       Name = "胴体"
+    },
+    Hand = {
+       Name = "手"
+    },
+    Ring = {
+       Name = "指"
+    },
+    Arm = {
+       Name = "腕"
+    },
+    Waist = {
+       Name = "腰"
+    },
+    Leg = {
+       Name = "足"
+    },
+    Ranged = {
+       Name = "遠隔"
+    },
+    Ammo = {
+       Name = "矢弾"
+    },
+}
