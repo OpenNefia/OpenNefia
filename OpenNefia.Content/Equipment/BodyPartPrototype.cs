2315b4f2ff4eb42568db5eb4794a9e15a1847a85
Ruin0x11 <ipickering2@gmail.com>
2022-01-06 12:01:32 -0800

    Add body parts

---
+++ OpenNefia.Content/Equipment/BodyPartPrototype.cs
@@ -0,0 +1,23 @@
+﻿using OpenNefia.Core.Prototypes;
+using OpenNefia.Core.Serialization.Manager.Attributes;
+using OpenNefia.Core.Rendering;
+
+namespace OpenNefia.Content.Equipment
+{
+    [Prototype("Elona.BodyPart")]
+    public class BodyPartPrototype : IPrototype
+    {
+        /// <inheritdoc />
+        [DataField("id", required: true)]
+        public string ID { get; private set; } = default!;
+
+        /// <summary>
+        /// Icon index for this body part.
+        /// </summary>
+        /// <remarks>
+        /// This is a region ID in <see cref="AssetPrototypeOf.BodyPartIcons"/>. 
+        /// </remarks>
+        [DataField]
+        public string Icon { get; } = "1";
+    }
+}
