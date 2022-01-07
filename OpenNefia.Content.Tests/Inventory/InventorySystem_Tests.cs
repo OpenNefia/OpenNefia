6e920fe980e5a6e86c09469389d386214d744552
Ruin0x11 <ipickering2@gmail.com>
2022-01-06 16:01:32 -0800

    Add/remove equip slot tests

--- OpenNefia.Content.Tests/Inventory/InventorySystem_Tests.cs
+++ OpenNefia.Content.Tests/Inventory/InventorySystem_Tests.cs
@@ -83,5 +83,94 @@ namespace OpenNefia.Content.Tests.Inventory
                 Assert.That((string)equipSlots[2].ContainerID, Is.EqualTo($"Elona.EquipSlot:TestSlot2:1"));
             });
         }
+
+        [Test]
+        public void TestAddEquipSlots()
+        {
+            var sim = SimulationFactory();
+
+            var entMan = sim.Resolve<IEntityManager>();
+            var mapMan = sim.Resolve<IMapManager>();
+
+            var containerSys = sim.GetEntitySystem<ContainerSystem>();
+            var invSys = sim.GetEntitySystem<InventorySystem>();
+
+             var map = sim.CreateMapAndSetActive(10, 10);
+
+            var ent = entMan.SpawnEntity(null, map.AtPos(Vector2i.One));
+
+            Assert.That(entMan.HasComponent<InventoryComponent>(ent), Is.False);
+            Assert.That(entMan.HasComponent<ContainerManagerComponent>(ent), Is.False);
+            var inventory = entMan.EnsureComponent<InventoryComponent>(ent);
+            var containers = entMan.EnsureComponent<ContainerManagerComponent>(ent);
+
+            Assert.Multiple(() =>
+            {
+                var oldContainerCount = containers.Containers.Count;
+
+                Assert.That(invSys.TryAddEquipSlot(ent, TestSlot1ID, out var containerSlot, out var equipSlot), Is.True);
+                Assert.That(invSys.HasEquipSlot(ent, TestSlot1ID), Is.True);
+
+                Assert.That(inventory.EquipSlots!.Count, Is.EqualTo(1), "Equip slot count");
+                Assert.That(containers.Containers.Count, Is.EqualTo(oldContainerCount + 1), "Container count");
+
+                Assert.That(equipSlot!.ID, Is.EqualTo(TestSlot1ID));
+                Assert.That((string)equipSlot.ContainerID, Is.EqualTo($"Elona.EquipSlot:TestSlot1:0"));
+                Assert.That(containerSys.HasContainer(ent, equipSlot.ContainerID), Is.True);
+            });
+        }
+
+        [Test]
+        public void TestRemoveEquipSlots()
+        {
+            var sim = SimulationFactory();
+
+            var entMan = sim.Resolve<IEntityManager>();
+            var mapMan = sim.Resolve<IMapManager>();
+
+            var containerSys = sim.GetEntitySystem<ContainerSystem>();
+            var invSys = sim.GetEntitySystem<InventorySystem>();
+
+            var map = sim.CreateMapAndSetActive(10, 10);
+
+            var ent = entMan.SpawnEntity(null, map.AtPos(Vector2i.One));
+            var entItem = entMan.SpawnEntity(null, map.AtPos(Vector2i.One));
+
+            Assert.That(entMan.HasComponent<InventoryComponent>(ent), Is.False);
+            Assert.That(entMan.HasComponent<ContainerManagerComponent>(ent), Is.False);
+
+            List<PrototypeId<EquipSlotPrototype>> equipSlotProtos = new()
+            {
+                TestSlot1ID,
+                TestSlot2ID,
+                TestSlot2ID,
+            };
+
+            var inventory = entMan.EnsureComponent<InventoryComponent>(ent);
+            var containers = entMan.EnsureComponent<ContainerManagerComponent>(ent);
+
+            invSys.InitializeEquipSlots(ent, equipSlotProtos);
+
+            var equipSlot = inventory.EquipSlots[0];
+            Assert.That(invSys.TryGetContainerForEquipSlot(ent, equipSlot, out var container), Is.True);
+
+            container!.Insert(entItem);
+
+            Assert.Multiple(() =>
+            {
+                var oldContainerCount = containers.Containers.Count;
+
+                Assert.That(invSys.TryRemoveEquipSlot(ent, equipSlot), Is.True);
+                Assert.That(invSys.HasEquipSlot(ent, TestSlot1ID), Is.False);
+
+                // Equipment in the slot should be deleted.
+                Assert.That(entMan.EntityExists(entItem), Is.False);
+
+                Assert.That(inventory.EquipSlots!.Count, Is.EqualTo(2), "Equip slot count");
+                Assert.That(containers.Containers.Count, Is.EqualTo(oldContainerCount - 1), "Container count");
+
+                Assert.That(invSys.TryRemoveEquipSlot(ent, equipSlot), Is.False);
+            });
+        }
     }
 }
