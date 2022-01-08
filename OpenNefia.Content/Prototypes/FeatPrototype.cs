e797a7838b9c1d81287d9c24f2bce2d57e78450d
kekobleibtkeko <alex.th101@gmail.com>
2022-01-08 04:52:52 +0100

    add feat prototypes

---
+++ OpenNefia.Content/Prototypes/FeatPrototype.cs
@@ -0,0 +1,34 @@
+﻿using OpenNefia.Core.Prototypes;
+using OpenNefia.Core.Serialization.Manager.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenNefia.Content.Prototypes
+{
+    public enum FeatType
+    {
+        Feat,
+        Race,
+        Mutation,
+        EtherDisease
+    }
+
+    [Prototype("Feat")]
+    public class FeatPrototype : IPrototype
+    {
+        [DataField("id", required: true)]
+        public string ID { get; } = default!;
+
+        [DataField]
+        public int LevelMin { get; }
+
+        [DataField]
+        public int LevelMax { get; }
+
+        [DataField]
+        public FeatType FeatType { get; } = FeatType.Feat;
+    }
+}
