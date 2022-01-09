7381b4c8b20ce340bb60c18303760626b530b46c
Ruin0x11 <ipickering2@gmail.com>
2022-01-09 12:07:18 -0800

    Reformat Protos classes

--- OpenNefia.Content/Prototypes/Protos/Protos.Music.cs
+++ OpenNefia.Content/Prototypes/Protos/Protos.Music.cs
@@ -2,59 +2,63 @@
 
 namespace OpenNefia.Content.Prototypes
 {
-	public static partial class Protos
-	{
-		public static class Music
-		{
-			public static readonly MusicPrototypeId Reset = new("Elona.Reset");
-			public static readonly MusicPrototypeId Town1 = new("Elona.Town1");
-			public static readonly MusicPrototypeId Town2 = new("Elona.Town2");
-			public static readonly MusicPrototypeId Town3 = new("Elona.Town3");
-			public static readonly MusicPrototypeId Town4 = new("Elona.Town4");
-			public static readonly MusicPrototypeId Dungeon1 = new("Elona.Dungeon1");
-			public static readonly MusicPrototypeId Dungeon2 = new("Elona.Dungeon2");
-			public static readonly MusicPrototypeId Dungeon3 = new("Elona.Dungeon3");
-			public static readonly MusicPrototypeId Dungeon4 = new("Elona.Dungeon4");
-			public static readonly MusicPrototypeId Dungeon5 = new("Elona.Dungeon5");
-			public static readonly MusicPrototypeId Dungeon6 = new("Elona.Dungeon6");
-			public static readonly MusicPrototypeId Puti = new("Elona.Puti");
-			public static readonly MusicPrototypeId Boss = new("Elona.Boss");
-			public static readonly MusicPrototypeId Boss2 = new("Elona.Boss2");
-			public static readonly MusicPrototypeId Victory = new("Elona.Victory");
-			public static readonly MusicPrototypeId Opening = new("Elona.Opening");
-			public static readonly MusicPrototypeId LastBoss = new("Elona.LastBoss");
-			public static readonly MusicPrototypeId Home = new("Elona.Home");
-			public static readonly MusicPrototypeId Lonely = new("Elona.Lonely");
-			public static readonly MusicPrototypeId Chaos = new("Elona.Chaos");
-			public static readonly MusicPrototypeId March1 = new("Elona.March1");
-			public static readonly MusicPrototypeId March2 = new("Elona.March2");
-			public static readonly MusicPrototypeId March3 = new("Elona.March3");
-			public static readonly MusicPrototypeId Arena = new("Elona.Arena");
-			public static readonly MusicPrototypeId Fanfare = new("Elona.Fanfare");
-			public static readonly MusicPrototypeId Village1 = new("Elona.Village1");
-			public static readonly MusicPrototypeId Battle1 = new("Elona.Battle1");
-			public static readonly MusicPrototypeId Casino = new("Elona.Casino");
-			public static readonly MusicPrototypeId Coda = new("Elona.Coda");
-			public static readonly MusicPrototypeId Ruin = new("Elona.Ruin");
-			public static readonly MusicPrototypeId Wedding = new("Elona.Wedding");
-			public static readonly MusicPrototypeId PetArena = new("Elona.PetArena");
-			public static readonly MusicPrototypeId Unrest = new("Elona.Unrest");
-			public static readonly MusicPrototypeId Town5 = new("Elona.Town5");
-			public static readonly MusicPrototypeId Unrest2 = new("Elona.Unrest2");
-			public static readonly MusicPrototypeId Town6 = new("Elona.Town6");
-			public static readonly MusicPrototypeId Field1 = new("Elona.Field1");
-			public static readonly MusicPrototypeId Field2 = new("Elona.Field2");
-			public static readonly MusicPrototypeId Field3 = new("Elona.Field3");
-			public static readonly MusicPrototypeId Memory = new("Elona.Memory");
-			public static readonly MusicPrototypeId Intro = new("Elona.Intro");
-			public static readonly MusicPrototypeId Soraochi = new("Elona.Soraochi");
-			public static readonly MusicPrototypeId Psml515 = new("Elona.Psml515");
-			public static readonly MusicPrototypeId Psml035 = new("Elona.Psml035");
-			public static readonly MusicPrototypeId Pinch2 = new("Elona.Pinch2");
-			public static readonly MusicPrototypeId Memory2 = new("Elona.Memory2");
-			public static readonly MusicPrototypeId AnoSora = new("Elona.AnoSora");
-			public static readonly MusicPrototypeId Epilogue = new("Elona.Epilogue");
-		}
-	}
+    public static partial class Protos
+    {
+        public static class Music
+        {
+            #pragma warning disable format
+
+            public static readonly MusicPrototypeId Reset    = new($"Elona.{nameof(Reset)}");
+            public static readonly MusicPrototypeId Town1    = new($"Elona.{nameof(Town1)}");
+            public static readonly MusicPrototypeId Town2    = new($"Elona.{nameof(Town2)}");
+            public static readonly MusicPrototypeId Town3    = new($"Elona.{nameof(Town3)}");
+            public static readonly MusicPrototypeId Town4    = new($"Elona.{nameof(Town4)}");
+            public static readonly MusicPrototypeId Dungeon1 = new($"Elona.{nameof(Dungeon1)}");
+            public static readonly MusicPrototypeId Dungeon2 = new($"Elona.{nameof(Dungeon2)}");
+            public static readonly MusicPrototypeId Dungeon3 = new($"Elona.{nameof(Dungeon3)}");
+            public static readonly MusicPrototypeId Dungeon4 = new($"Elona.{nameof(Dungeon4)}");
+            public static readonly MusicPrototypeId Dungeon5 = new($"Elona.{nameof(Dungeon5)}");
+            public static readonly MusicPrototypeId Dungeon6 = new($"Elona.{nameof(Dungeon6)}");
+            public static readonly MusicPrototypeId Puti     = new($"Elona.{nameof(Puti)}");
+            public static readonly MusicPrototypeId Boss     = new($"Elona.{nameof(Boss)}");
+            public static readonly MusicPrototypeId Boss2    = new($"Elona.{nameof(Boss2)}");
+            public static readonly MusicPrototypeId Victory  = new($"Elona.{nameof(Victory)}");
+            public static readonly MusicPrototypeId Opening  = new($"Elona.{nameof(Opening)}");
+            public static readonly MusicPrototypeId LastBoss = new($"Elona.{nameof(LastBoss)}");
+            public static readonly MusicPrototypeId Home     = new($"Elona.{nameof(Home)}");
+            public static readonly MusicPrototypeId Lonely   = new($"Elona.{nameof(Lonely)}");
+            public static readonly MusicPrototypeId Chaos    = new($"Elona.{nameof(Chaos)}");
+            public static readonly MusicPrototypeId March1   = new($"Elona.{nameof(March1)}");
+            public static readonly MusicPrototypeId March2   = new($"Elona.{nameof(March2)}");
+            public static readonly MusicPrototypeId March3   = new($"Elona.{nameof(March3)}");
+            public static readonly MusicPrototypeId Arena    = new($"Elona.{nameof(Arena)}");
+            public static readonly MusicPrototypeId Fanfare  = new($"Elona.{nameof(Fanfare)}");
+            public static readonly MusicPrototypeId Village1 = new($"Elona.{nameof(Village1)}");
+            public static readonly MusicPrototypeId Battle1  = new($"Elona.{nameof(Battle1)}");
+            public static readonly MusicPrototypeId Casino   = new($"Elona.{nameof(Casino)}");
+            public static readonly MusicPrototypeId Coda     = new($"Elona.{nameof(Coda)}");
+            public static readonly MusicPrototypeId Ruin     = new($"Elona.{nameof(Ruin)}");
+            public static readonly MusicPrototypeId Wedding  = new($"Elona.{nameof(Wedding)}");
+            public static readonly MusicPrototypeId PetArena = new($"Elona.{nameof(PetArena)}");
+            public static readonly MusicPrototypeId Unrest   = new($"Elona.{nameof(Unrest)}");
+            public static readonly MusicPrototypeId Town5    = new($"Elona.{nameof(Town5)}");
+            public static readonly MusicPrototypeId Unrest2  = new($"Elona.{nameof(Unrest2)}");
+            public static readonly MusicPrototypeId Town6    = new($"Elona.{nameof(Town6)}");
+            public static readonly MusicPrototypeId Field1   = new($"Elona.{nameof(Field1)}");
+            public static readonly MusicPrototypeId Field2   = new($"Elona.{nameof(Field2)}");
+            public static readonly MusicPrototypeId Field3   = new($"Elona.{nameof(Field3)}");
+            public static readonly MusicPrototypeId Memory   = new($"Elona.{nameof(Memory)}");
+            public static readonly MusicPrototypeId Intro    = new($"Elona.{nameof(Intro)}");
+            public static readonly MusicPrototypeId Soraochi = new($"Elona.{nameof(Soraochi)}");
+            public static readonly MusicPrototypeId Psml515  = new($"Elona.{nameof(Psml515)}");
+            public static readonly MusicPrototypeId Psml035  = new($"Elona.{nameof(Psml035)}");
+            public static readonly MusicPrototypeId Pinch2   = new($"Elona.{nameof(Pinch2)}");
+            public static readonly MusicPrototypeId Memory2  = new($"Elona.{nameof(Memory2)}");
+            public static readonly MusicPrototypeId AnoSora  = new($"Elona.{nameof(AnoSora)}");
+            public static readonly MusicPrototypeId Epilogue = new($"Elona.{nameof(Epilogue)}");
+
+            #pragma warning restore format
+        }
+    }
 
 }
