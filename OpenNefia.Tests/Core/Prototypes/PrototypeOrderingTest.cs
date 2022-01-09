1126ac356b906498e21376a690f9255978767e80
Ruin0x11 <ipickering2@gmail.com>
2022-01-09 11:24:15 -0800

    Add topological ordering to prototypes

---
+++ OpenNefia.Tests/Core/Prototypes/PrototypeOrderingTest.cs
@@ -0,0 +1,120 @@
+﻿using NUnit.Framework;
+using OpenNefia.Core.GameObjects;
+using OpenNefia.Core.IoC;
+using OpenNefia.Core.Maths;
+using OpenNefia.Core.Prototypes;
+using OpenNefia.Core.Serialization.Manager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenNefia.Tests.Core.Prototypes
+{
+    [TestFixture]
+    public class PrototypeOrderingTest : OpenNefiaUnitTest
+    {
+        private IPrototypeManager manager = default!;
+
+
+        private static readonly PrototypeId<EntityPrototype> TestProto1ID = new("TestProto1");
+        private static readonly PrototypeId<EntityPrototype> TestProto2ID = new("TestProto2");
+        private static readonly PrototypeId<EntityPrototype> TestProto3ID = new("TestProto3");
+        private static readonly PrototypeId<EntityPrototype> TestProto4ID = new("TestProto4");
+
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+            var factory = IoCManager.Resolve<IComponentFactory>();
+            factory.RegisterClass<TestBasicPrototypeComponent>();
+
+            IoCManager.Resolve<ISerializationManager>().Initialize();
+            manager = IoCManager.Resolve<IPrototypeManager>();
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            manager.Clear();
+            manager.RegisterType<EntityPrototype>();
+        }
+
+        [Test]
+        public void TestOrderingBefore()
+        {
+            var prototypes = @$"
+- type: Entity
+  id: {TestProto1ID}
+
+- type: Entity
+  id: {TestProto2ID}
+  ordering:
+    before: {TestProto1ID}
+";
+
+            manager.LoadString(prototypes);
+            manager.Resync();
+
+            var enumerator = manager.EnumeratePrototypes<EntityPrototype>()
+                .Select(p => p.GetStrongID());
+
+            Assert.That(enumerator, Is.EquivalentTo(new[] { TestProto2ID, TestProto1ID }));
+        }
+
+        [Test]
+        public void TestOrderingAfter()
+        {
+            var prototypes = @$"
+- type: Entity
+  id: {TestProto1ID}
+  ordering:
+    after: {TestProto2ID}
+
+- type: Entity
+  id: {TestProto2ID}
+";
+
+            manager.LoadString(prototypes);
+            manager.Resync();
+
+            var enumerator = manager.EnumeratePrototypes<EntityPrototype>()
+                .Select(p => p.GetStrongID());
+
+            Assert.That(enumerator, Is.EquivalentTo(new[] { TestProto2ID, TestProto1ID }));
+        }
+
+        [Test]
+        public void TestOrderingAutomatic()
+        {
+            var prototypes1 = @$"
+- type: Entity
+  id: {TestProto1ID}
+  ordering:
+    after: {TestProto4ID}
+
+# Should be automatically ordered after TestProto1 above.
+- type: Entity
+  id: {TestProto2ID}
+";
+
+            var prototypes2 = @$"
+- type: Entity
+  id: {TestProto3ID}
+
+# Should be automatically ordered after TestProto3 above.
+- type: Entity
+  id: {TestProto4ID}
+";
+
+            manager.LoadString(prototypes1);
+            manager.LoadString(prototypes2);
+            manager.Resync();
+
+            var enumerator = manager.EnumeratePrototypes<EntityPrototype>()
+                .Select(p => p.GetStrongID());
+
+            Assert.That(enumerator, Is.EquivalentTo(new[] { TestProto3ID, TestProto4ID, TestProto1ID, TestProto2ID }));
+        }
+    }
+}
