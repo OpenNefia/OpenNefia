cef81963006ffaa4d27b7c0cfaaa4e727c29e3c8
Ruin0x11 <ipickering2@gmail.com>
2022-01-08 21:53:19 -0800

    JP translations: CharaMake.lua

---
+++ OpenNefia.Content/Resources/Locale/ja_JP/Elona/CharaMake/CharaMake.lua
@@ -0,0 +1,86 @@
+Elona.CharaMake = {
+	ProficientIn = "武器の専門",
+	Extra = "(extra)",
+	Skill = {
+		Amount = {
+			None = "皆無",
+			Slight = "微々",
+			Little = "少し",
+			Normal = "普通",
+			NotBad = "やや高い",
+			Good = "高い",
+			Great = "理想的",
+			Best = "最高",
+		},
+	},
+	RaceSelect = {
+		Window = {
+			Title = "種族の選択",
+		},
+
+		DetailTopic = "種族の説明",
+		RaceTopic = "選択できる種族",
+
+		AttributeTopic = "能力ボーナス",
+		SkillTopic = "獲得技能",
+		Caption = "やあ、待っていたよ。早速旅の支度をしようか。"
+	},
+
+	ClassSelect = {
+		Window = {
+			Title = "職業の選択",
+		},
+
+		DetailTopic = "職業の説明",
+		RaceTopic = "選択できる職業",
+
+		AttributeTopic = "能力ボーナス",
+		SkillTopic = "獲得技能",
+		Caption = "職業や種族は、初期の能力だけでなく、成長の方向性に影響するんだ。",
+		RaceLabel = "種族"
+	},
+
+	AliasSelect = {
+		Window = {
+			Title = "異名の選択"
+		},
+		Reroll = "リロール",
+		AliasTopic = "異名の候補",
+		Caption = "有名になると、名前とは別の通り名で呼ばれることがあるらしい。"
+	},
+
+	GenderSelect = {
+		Window = {
+			Title = "性別の選択"
+		},
+		GenderTopic = "性別の候補",
+		Caption = "男性と女性に能力の違いはない。"
+	},
+
+	AttributeReroll = {
+		Window = {
+			Title = "能力のロール"
+		},
+		AttrTopic = "能力",
+		AttrInfo = "ロックされた能力は\n変化しません",
+		LockAmt = "残りロック",
+		Reroll = "リロール",
+		Proceed = "決定",
+		Locked = "Locked!",
+		Caption = "死にたくないなら、ある程度の能力は必要だね。"
+	},
+
+	FeatSelect = {
+		Window = {
+			Title = "特徴と体質"
+		},
+		Caption = "フィートとは、君の持っている有益な特徴だ。3つまで選べるよ。"
+	},
+
+	AppearanceSelect = {
+		Window = {
+			Title = "肖像の変更"
+		},
+		Caption = "君の見た目を知っておきたいな。まあ、後からいつでも変えられるけどね。"
+	},
+}
