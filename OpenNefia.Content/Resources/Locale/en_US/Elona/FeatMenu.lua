d087ee4b9a18f7732b0fe366ecbf1af9a2e20439
kekobleibtkeko <alex.th101@gmail.com>
2022-01-09 06:00:00 +0100

    feat menu improvements
     - can only gain feats up to the maximum level
     - text color changes based on feat level
     - added display for feat detail
     - fixed layout

--- OpenNefia.Content/Resources/Locale/en_US/Elona/FeatMenu.lua
+++ OpenNefia.Content/Resources/Locale/en_US/Elona/FeatMenu.lua
@@ -2,7 +2,10 @@
 	Title = "Feats and Traits",
 	GainedHeader = "[Feats and traits]",
 	AvailableHeader = "[Available feats]",
-	
+	FeatMax = "MAX",
+	NameTopic = "Name",
+	DetailTopic = "Detail",
+
 	FeatType = {
 		Feat = "Feat",
         Race = "Race",
