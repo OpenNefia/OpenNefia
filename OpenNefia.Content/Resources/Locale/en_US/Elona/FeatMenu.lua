d997c3e0cde24c9e1f46e2ce57fc199561066c4e
Ruin0x11 <ipickering2@gmail.com>
2022-01-11 17:03:20 -0800

    precommit run --all again...

--- OpenNefia.Content/Resources/Locale/en_US/Elona/FeatMenu.lua
+++ OpenNefia.Content/Resources/Locale/en_US/Elona/FeatMenu.lua
@@ -1,19 +1,19 @@
-﻿Elona.FeatMenu = {
-	Title = "Feats and Traits",
-	GainedHeader = "[Feats and traits]",
-	AvailableHeader = "[Available feats]",
-	FeatMax = "MAX",
-	Topic = {
-		Name = "Name",
-		Detail = "Detail",
-	},
-	FeatCount = function(featsRemaining)
-		return ("You can acquire %s feats"):format(featsRemaining)
-	end,
-	FeatType = {
-		Feat = "Feat",
+Elona.FeatMenu = {
+    Title = "Feats and Traits",
+    GainedHeader = "[Feats and traits]",
+    AvailableHeader = "[Available feats]",
+    FeatMax = "MAX",
+    Topic = {
+        Name = "Name",
+        Detail = "Detail",
+    },
+    FeatCount = function(featsRemaining)
+        return ("You can acquire %s feats"):format(featsRemaining)
+    end,
+    FeatType = {
+        Feat = "Feat",
         Race = "Race",
         Mutation = "Mutation",
-        EtherDisease = "Disease"
-	},
+        EtherDisease = "Disease",
+    },
 }
