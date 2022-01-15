00bed9da153b84f608fc39486be71771ad7bf05a
kekobleibtkeko <alex.th101@gmail.com>
2022-01-15 04:02:04 +0100

    add regions for message window

---
+++ OpenNefia.Content/UI/Element/UiMessageWindowBacking.cs
@@ -0,0 +1,31 @@
+﻿using OpenNefia.Core.IoC;
+using OpenNefia.Core.Rendering;
+using OpenNefia.Core.UI.Element;
+using OpenNefia.Content.Prototypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenNefia.Content.UI.Element
+{
+    public class UiMessageWindowBacking : BaseDrawable
+    {
+        private IAssetInstance MessageWindowAsset;
+        public UiMessageWindowBacking()
+        {
+            MessageWindowAsset = Assets.Get(Protos.Asset.MessageWindow);
+        }
+
+        public override void Draw()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(float dt)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
