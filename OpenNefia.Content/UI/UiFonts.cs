2d1081bcac7d9439d9de310e8dc1e355f5a59325
Ruin0x11 <ipickering2@gmail.com>
2022-01-16 12:57:57 -0800

    Character sheet font fixes for Japanese

--- OpenNefia.Content/UI/UiFonts.cs
+++ OpenNefia.Content/UI/UiFonts.cs
@@ -24,8 +24,8 @@ namespace OpenNefia.Content.UI
         public static readonly FontSpec CharaMakeRerollAttrAmount = new(14, 12, style: FontStyle.Bold);
         public static readonly FontSpec CharaMakeRerollLocked = new(11, 9, color: UiColors.CharaMakeAttrLevelGreat, style: FontStyle.Bold);
 
-        public static readonly FontSpec CharSheetInfo = new(11, 11);
-        public static readonly FontSpec CharaSheetInfoContent = new(12, 12);
+        public static readonly FontSpec CharSheetInfo = new(13, 11);
+        public static readonly FontSpec CharaSheetInfoContent = new(14, 12);
 
         public static readonly FontSpec TextNote = new(12, 10, style: FontStyle.Bold); // 12 + sizefix - en * 2
 
