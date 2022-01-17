c28b305f1e707e66f16af1588b8127d7979ac39d
Ruin0x11 <ipickering2@gmail.com>
2022-01-17 14:33:02 -0800

    Serialize tile memory

--- OpenNefia.Content/ConsoleCommands/ConvertHspMapCommand.cs
+++ OpenNefia.Content/ConsoleCommands/ConvertHspMapCommand.cs
@@ -213,7 +213,7 @@ namespace OpenNefia.Content.ConsoleCommands
                 }
             }
 
-            var protoToRune = YamlGridSerializer.BuildProtoToRuneTileMap(map);
+            var protoToRune = YamlGridSerializer.BuildProtoToRuneTileMap(map, _prototypeManager);
 
             var grid = YamlGridSerializer.SerializeGrid(map.Tiles, map.Size, protoToRune, _tileDefinitionManager);
             var runeToProtoTileMap = new YamlMappingNode();
