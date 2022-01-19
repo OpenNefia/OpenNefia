ca5ee3ed8dda9210ad071f985014a0eb7d571297
kekobleibtkeko <alex.th101@gmail.com>
2022-01-19 21:31:34 +0100

    add dialog prototype

---
+++ OpenNefia.Content/Dialog/DialogItemPrototype.cs
@@ -0,0 +1,17 @@
+﻿using OpenNefia.Core.Prototypes;
+using OpenNefia.Core.Serialization.Manager.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenNefia.Content.Dialog
+{
+    [Prototype("Elona.DialogItem")]
+    public sealed class DialogItemPrototype : IPrototype
+    {
+        [DataField("id", required: true)]
+        public string ID { get; } = default!;
+    }
+}
