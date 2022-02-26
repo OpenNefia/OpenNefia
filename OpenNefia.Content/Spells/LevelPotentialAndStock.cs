1967bdfbf155215550bf0cbfdc26a6b240874238
Ruin0x11 <ipickering2@gmail.com>
2022-02-25 22:01:43 -0800

    Move spell-related classes to their own namespace

---
+++ OpenNefia.Content/Spells/LevelPotentialAndStock.cs
@@ -0,0 +1,37 @@
+﻿using OpenNefia.Content.Skills;
+using OpenNefia.Core.Serialization.Manager.Attributes;
+
+namespace OpenNefia.Content.Spells
+{
+    /// <summary>
+    /// The data associated with a spell.
+    /// </summary>
+    [DataDefinition]
+    public class LevelPotentialAndStock : IEquatable<LevelPotentialAndStock>
+    {
+        /// <summary>
+        /// The level, potential and experience associated with this spell.
+        /// </summary>
+        [DataField]
+        public LevelAndPotential Stats { get; } = new();
+
+        /// <summary>
+        /// Current spell stock.
+        /// </summary>
+        [DataField]
+        public int SpellStock { get; set; } = 0;
+
+        public bool Equals(LevelPotentialAndStock? other)
+        {
+            if (other == null)
+                return false;
+
+            return Stats.Equals(other.Stats) && SpellStock == other.SpellStock;
+        }
+
+        public override string ToString()
+        {
+            return $"(Level={Stats.Level} Potential={Stats.Potential}% EXP={Stats.Experience} Stock={SpellStock})";
+        }
+    }
+}
--- OpenNefia.Content/Spells/LevelPotentialAndStockSerializer.cs
+++ OpenNefia.Content/Spells/LevelPotentialAndStockSerializer.cs
@@ -1,7 +1,5 @@
 ﻿using OpenNefia.Content.Skills;
-using OpenNefia.Core.GameObjects;
 using OpenNefia.Core.IoC;
-using OpenNefia.Core.Prototypes;
 using OpenNefia.Core.Serialization.Manager;
 using OpenNefia.Core.Serialization.Manager.Attributes;
 using OpenNefia.Core.Serialization.Manager.Result;
@@ -9,73 +7,9 @@ using OpenNefia.Core.Serialization.Markdown;
 using OpenNefia.Core.Serialization.Markdown.Validation;
 using OpenNefia.Core.Serialization.Markdown.Value;
 using OpenNefia.Core.Serialization.TypeSerializers.Interfaces;
-using OpenNefia.Core.Stats;
-using System.Diagnostics.CodeAnalysis;
 
-namespace OpenNefia.Content.GameObjects
+namespace OpenNefia.Content.Spells
 {
-    /// <summary>
-    /// Holds spell level/stock data.
-    /// </summary>
-    [RegisterComponent]
-    public class SpellsComponent : Component
-    {
-        public override string Name => "Spells";
-
-        /// <summary>
-        /// Level, potential, experience and spell stock for spells.
-        /// </summary>
-        [DataField]
-        public Dictionary<PrototypeId<SkillPrototype>, LevelPotentialAndStock> Spells { get; } = new();
-
-        public bool TryGetKnown(PrototypeId<SkillPrototype> protoId, [NotNullWhen(true)] out LevelPotentialAndStock? level)
-        {
-            if (!Spells.TryGetValue(protoId, out level))
-            {
-                return false;
-            }
-
-            if (level.Stats.Level.Base <= 0)
-            {
-                return false;
-            }
-
-            return true;
-        }
-    }
-
-    /// <summary>
-    /// The data associated with a spell.
-    /// </summary>
-    [DataDefinition]
-    public class LevelPotentialAndStock : IEquatable<LevelPotentialAndStock>
-    {
-        /// <summary>
-        /// The level, potential and experience associated with this spell.
-        /// </summary>
-        [DataField]
-        public LevelAndPotential Stats { get; } = new();
-
-        /// <summary>
-        /// Current spell stock.
-        /// </summary>
-        [DataField]
-        public int SpellStock { get; set; } = 0;
-
-        public bool Equals(LevelPotentialAndStock? other)
-        {
-            if (other == null)
-                return false;
-
-            return Stats.Equals(other.Stats) && SpellStock == other.SpellStock;
-        }
-
-        public override string ToString()
-        {
-            return $"(Level={Stats.Level} Potential={Stats.Potential}% EXP={Stats.Experience} Stock={SpellStock})";
-        }
-    }
-
     [TypeSerializer]
     public class LevelPotentialAndStockSerializer : ITypeSerializer<LevelPotentialAndStock, ValueDataNode>
     {
