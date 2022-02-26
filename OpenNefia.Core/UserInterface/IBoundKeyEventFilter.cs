dd0e70ea337df78597d91a0e83ee46a242845fe7
Ruin0x11 <ipickering2@gmail.com>
2022-02-25 22:02:00 -0800

    Key event filters
    Modeled after Qt's event filters.

---
+++ OpenNefia.Core/UserInterface/IBoundKeyEventFilter.cs
@@ -0,0 +1,14 @@
+﻿using OpenNefia.Core.UI.Element;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenNefia.Core.UserInterface
+{
+    public interface IBoundKeyEventFilter
+    {
+        bool FilterEvent(UiElement element, GUIBoundKeyEventArgs evt);
+    }
+}
