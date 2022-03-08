6f1d21dc141e665fef2907346a79ac51f643bda6
Ruin0x11 <ipickering2@gmail.com>
2022-03-07 23:35:02 -0800

    Editor templates for XAML

---
+++ EditorSupport/VSTemplates/Window/Window.xaml.cs
@@ -0,0 +1,15 @@
+﻿using OpenNefia.Core.UserInterface;
+using OpenNefia.Core.UserInterface.XAML;
+using OpenNefia.Core.UI.Wisp.Controls;
+using OpenNefia.Core.UI.Wisp.CustomControls;
+
+namespace $rootnamespace$
+{
+    public partial class $safeitemrootname$ : DefaultWindow
+    {
+        public $safeitemrootname$()
+        {
+            OpenNefiaXamlLoader.Load(this);
+        }
+    }
+}
