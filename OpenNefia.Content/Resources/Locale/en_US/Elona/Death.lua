e7af3c5a6a274b4f16e10a8451b1b03b36ad4cca
Ruin0x11 <ipickering2@gmail.com>
2022-06-16 23:17:38 -0700

    last words translation

---
+++ OpenNefia.Content/Resources/Locale/en_US/Elona/Death.lua
@@ -0,0 +1,16 @@
+Elona.Death = {
+    GoodBye = "Good bye... ",
+    PromptDyingMessage = "You leave a dying message.",
+    MessageQuotes = function(message)
+        return ("\"%s\""):format(message)
+    end,
+    DefaultLastWords = {
+        "Oh my god!",
+        "What happened?",
+        "Aww-wwww!",
+        "How llama!",
+        "Phony slob!",
+        "Rest in peace.",
+        "Scut!",
+    },
+}
