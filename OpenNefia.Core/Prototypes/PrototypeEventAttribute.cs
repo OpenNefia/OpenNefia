f857e4bdf2f8f8dfc720ee28eb0d7ca52fe0d4ef
Ruin0x11 <ipickering2@gmail.com>
2022-06-28 23:22:44 -0700

    misc cleanup

---
+++ OpenNefia.Core/Prototypes/PrototypeEventAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenNefia.Core.Prototypes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+    public class PrototypeEventAttribute : Attribute
+    {
+        public Type PrototypeType { get; }
+
+        public PrototypeEventAttribute(Type prototypeType)
+        {
+            PrototypeType = prototypeType;
+        }
+    }
+}
