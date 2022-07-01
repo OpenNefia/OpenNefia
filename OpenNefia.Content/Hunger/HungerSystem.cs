1028400a7a4b6d6fafb5cf82a01afba0f6fc687e
Ruin0x11 <ipickering2@gmail.com>
2022-07-01 04:53:07 -0700

    Nutrition initialization

--- OpenNefia.Content/Hunger/HungerSystem.cs
+++ OpenNefia.Content/Hunger/HungerSystem.cs
@@ -1,6 +1,8 @@
-﻿using OpenNefia.Content.Logic;
+﻿using OpenNefia.Content.EntityGen;
+using OpenNefia.Content.Logic;
 using OpenNefia.Content.Prototypes;
 using OpenNefia.Core.Areas;
+using OpenNefia.Core.Game;
 using OpenNefia.Core.GameObjects;
 using OpenNefia.Core.IoC;
 using OpenNefia.Core.Locale;
@@ -26,12 +28,22 @@ namespace OpenNefia.Content.Hunger
         [Dependency] private readonly IRandom _rand = default!;
         [Dependency] private readonly IMessagesManager _mes = default!;
         [Dependency] private readonly IEntityLookup _lookup = default!;
+        [Dependency] private readonly IGameSessionManager _gameSession = default!;
 
         public override void Initialize()
         {
+            SubscribeComponent<HungerComponent, EntityBeingGeneratedEvent>(InitializeNutrition, priority: EventPriorities.High);
         }
 
-        public const int NutritionThresholdVomit = 35000;
+        private void InitializeNutrition(EntityUid uid, HungerComponent component, ref EntityBeingGeneratedEvent args)
+        {
+            // >>>>>>>> shade2/chara.hsp:516 	if rc=pc:cHunger(rc)=9000:else:cHunger(rc)=defAll ..
+            if (_gameSession.IsPlayer(uid))
+                component.Nutrition = 9000;
+            else
+                component.Nutrition = HungerLevels.Ally - 1000 + _rand.Next(4000);
+            // <<<<<<<< shade2/chara.hsp:516 	if rc=pc:cHunger(rc)=9000:else:cHunger(rc)=defAll ..
+        }
 
         public void Vomit(EntityUid entity, HungerComponent? hunger = null)
         {
