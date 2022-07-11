384c1a9f734abd5b0fe105269a7d28e6a2807ffb
Ruin0x11 <ipickering2@gmail.com>
2022-07-11 13:32:08 -0700

    SerializationManager.CanSerializeType() should support generics

---
+++ OpenNefia.Tests/Core/Serialization/SerializationManagerTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using OpenNefia.Core.Maps;
+using OpenNefia.Core.Serialization.Manager;
+
+namespace OpenNefia.Tests.Core.Serialization
+{
+    [TestFixture]
+    [TestOf(typeof(SerializationManager))]
+    public sealed class SerializationManagerTest : SerializationTest
+    {
+        [Test]
+        public void TestCanSerializeType()
+        {
+            Assert.That(Serialization.CanSerializeType(typeof(string)), Is.True);
+            Assert.That(Serialization.CanSerializeType(typeof(EntityCoordinates)), Is.True);
+            Assert.That(Serialization.CanSerializeType(typeof(List<string>)), Is.True);
+        }
+    }
+}
