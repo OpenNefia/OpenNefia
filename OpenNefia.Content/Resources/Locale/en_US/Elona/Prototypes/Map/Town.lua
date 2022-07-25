fdd495670a4604fc4562f072cd5ee96b0a159a1c
Ruin0x11 <ipickering2@gmail.com>
2022-07-24 19:23:14 -0700

    Localization string references

--- OpenNefia.Content/Resources/Locale/en_US/Elona/Prototypes/Map/Town.lua
+++ OpenNefia.Content/Resources/Locale/en_US/Elona/Prototypes/Map/Town.lua
@@ -1,7 +1,16 @@
 OpenNefia.Prototypes.Entity.Elona = {
-    MapPalmia = {
+    AreaPalmia = {
         MetaData = {
             Name = "Palmia",
+        },
+        AreaEntrance = {
+            EntranceMessage = "You see the great city of Palmia. Entire city is surrounded by tall wall.",
+        },
+    },
+
+    MapPalmia = {
+        MetaData = {
+            Name = _.refp "Entity.Elona.AreaPalmia.MetaData.Name",
         },
     },
 }
--- OpenNefia.Content/Resources/Locale/ja_JP/Elona/Prototypes/Map/World.lua
+++ OpenNefia.Content/Resources/Locale/ja_JP/Elona/Prototypes/Map/World.lua
@@ -1,4 +1,10 @@
 OpenNefia.Prototypes.Entity.Elona = {
+    AreaNorthTyris = {
+        MetaData = {
+            Name = "ノースティリス",
+        },
+    },
+
     MapNorthTyris = {
         MetaData = {
             Name = "ノースティリス",
