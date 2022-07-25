b6eab132f9f8998b14459db12dd6a954e7cc4ee5
Ruin0x11 <ipickering2@gmail.com>
2022-07-25 02:26:13 -0700

    Add town area localizations

---
+++ OpenNefia.Content/Resources/Locale/ja_JP/Elona/Prototypes/Area/Town.lua
@@ -0,0 +1,64 @@
+OpenNefia.Prototypes.Entity.Elona = {
+    AreaVernis = {
+        MetaData = {
+            Name = "ヴェルニース",
+        },
+        AreaEntrance = {
+            EntranceMessage = "ヴェルニースの街が見える。辺りは活気に満ちている。",
+        },
+    },
+
+    AreaYowyn = {
+        MetaData = {
+            Name = "ヨウィン",
+        },
+        AreaEntrance = {
+            EntranceMessage = "ヨウィンの村が見える。懐かしい土の匂いがする。",
+        },
+    },
+
+    AreaPalmia = {
+        MetaData = {
+            Name = "パルミア",
+        },
+        AreaEntrance = {
+            EntranceMessage = "パルミアの都がある。都は高い壁に囲われている。",
+        },
+    },
+
+    AreaDerphy = {
+        MetaData = {
+            Name = "ダルフィ",
+        },
+        AreaEntrance = {
+            EntranceMessage = "ダルフィの街がある。何やら危険な香りがする。",
+        },
+    },
+
+    AreaPortKapul = {
+        MetaData = {
+            Name = "ポート・カプール",
+        },
+        AreaEntrance = {
+            EntranceMessage = "ポート・カプールが見える。港は船で賑わっている。",
+        },
+    },
+
+    AreaNoyel = {
+        MetaData = {
+            Name = "ノイエル",
+        },
+        AreaEntrance = {
+            EntranceMessage = "ノイエルの村がある。子供たちの笑い声が聞こえる。",
+        },
+    },
+
+    AreaLumiest = {
+        MetaData = {
+            Name = "ルミエスト",
+        },
+        AreaEntrance = {
+            EntranceMessage = "ルミエストの都が見える。水のせせらぎが聴こえる。",
+        },
+    },
+}
