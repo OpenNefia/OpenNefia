297fffb392be79ba54a3932a77c6b1d8be28d640
Ruin0x11 <ipickering2@gmail.com>
2022-07-28 23:10:20 -0700

    Treasure map component

---
+++ OpenNefia.Content/TreasureMap/TreasureMapComponent.cs
@@ -0,0 +1,23 @@
+﻿using OpenNefia.Content.Prototypes;
+using OpenNefia.Core.GameObjects;
+using OpenNefia.Core.Maps;
+using OpenNefia.Core.Prototypes;
+using OpenNefia.Core.Serialization.Manager.Attributes;
+using System;
+using System.Collections.Generic;
+
+namespace OpenNefia.Content.TreasureMap
+{
+    [RegisterComponent]
+    [ComponentUsage(ComponentTarget.Normal)]
+    public sealed class TreasureMapComponent : Component
+    {
+        public override string Name => "TreasureMap";
+
+        /// <summary>
+        /// Place the treasure is located.
+        /// </summary>
+        [DataField]
+        public MapCoordinates TreasureCoords { get; set; }
+    }
+}
