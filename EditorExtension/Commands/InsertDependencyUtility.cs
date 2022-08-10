778378d7ac98dc8c74032ea5dd8e5574b355217b
Ruin0x11 <ipickering2@gmail.com>
2022-08-10 08:30:09 -0700

    Add editor extension
    For quickly inserting [Dependency] fields and such.

---
+++ EditorExtension/Commands/InsertDependencyUtility.cs
@@ -0,0 +1,48 @@
+﻿using Humanizer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static OpenNefia.EditorExtension.InsertDependencyCommandPackage;
+
+namespace OpenNefia.EditorExtension
+{
+    public static class InsertDependencyUtility
+    {
+        private static Regex EntitySystemRegex = new Regex("^(I)?(.*)System$");
+        private static Regex ManagerRegex = new Regex("^(I)?(.*)Manager$");
+        private static Regex GeneralTypeRegex = new Regex("^(I)?(.*)$");
+
+        public static string GetDefaultPropertyNameForType(string shortName)
+        {
+            var match = ManagerRegex.Match(shortName);
+            if (match.Success)
+            {
+                return "_" + match.Groups[2].Value.FirstCharToLowerCase();
+            }
+
+            foreach (var regex in new Regex[] { EntitySystemRegex, GeneralTypeRegex })
+            {
+                match = regex.Match(shortName);
+                if (match.Success)
+                {
+                    // ISkillSystem -> _skills
+                    var noun = match.Groups[2].Value.FirstCharToLowerCase();
+                    return $"_{noun.Pluralize()}";
+                }
+            }
+
+            return shortName.FirstCharToLowerCase();
+        }
+
+        private static string FirstCharToLowerCase(this string str)
+        {
+            if (!string.IsNullOrEmpty(str) && char.IsUpper(str[0]))
+                return str.Length == 1 ? char.ToLower(str[0]).ToString() : char.ToLower(str[0]) + str.Substring(1);
+
+            return str;
+        }
+    }
+}
---
+++ EditorExtension/Utility/KeyboardFocus.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace OpenNefia.EditorExtension
+{
+    public static class KeyboardFocus
+    {
+        public static readonly DependencyProperty OnProperty;
+
+        public static void SetOn(UIElement element, FrameworkElement value)
+        {
+            element.SetValue(OnProperty, value);
+        }
+
+        public static FrameworkElement GetOn(UIElement element)
+        {
+            return (FrameworkElement)element.GetValue(OnProperty);
+        }
+
+        static KeyboardFocus()
+        {
+            OnProperty = DependencyProperty.RegisterAttached("On", typeof(FrameworkElement), typeof(KeyboardFocus), new PropertyMetadata(OnSetCallback));
+        }
+
+        private static void OnSetCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            var frameworkElement = (FrameworkElement)dependencyObject;
+            var target = GetOn(frameworkElement);
+
+            if (target == null)
+                return;
+
+            frameworkElement.Loaded += (s, e) => Keyboard.Focus(target);
+        }
+    }
+}
