da4688cd8aefc5fa84dfc5789d4f79d667af72c1
Ruin0x11 <ipickering2@gmail.com>
2022-08-17 00:26:54 -0700

    Fix stacking message

--- OpenNefia.Content/Resources/Locale/en_US/Elona/GameObjects/Stack.lua
+++ OpenNefia.Content/Resources/Locale/en_US/Elona/GameObjects/Stack.lua
@@ -1,5 +1,5 @@
 Elona.GameObjects.Stack = {
     HasBeenStacked = function(entity, totalCount)
-        return ("%s has been stacked. (Total:%s)"):format(entity, totalCount)
+      return ("%s has been stacked. (Total:%s)"):format(_.name(entity), totalCount)
     end,
 }
