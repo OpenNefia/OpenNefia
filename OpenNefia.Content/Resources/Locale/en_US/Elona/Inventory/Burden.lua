8728c4d054b5ea215e62a9e757f70817c263b9e7
Ruin0x11 <ipickering2@gmail.com>
2022-08-17 00:33:45 -0700

    Fix burden calculation

--- OpenNefia.Content/Resources/Locale/en_US/Elona/Inventory/Burden.lua
+++ OpenNefia.Content/Resources/Locale/en_US/Elona/Inventory/Burden.lua
@@ -1,7 +1,7 @@
 Elona.Inventory.Burden = {
     CarryTooMuch = "You carry too much to move!",
     BackpackSquashing = function(entity)
-        return ("%s%s backpack is squashing %s!"):format(_.name(entity), _.possessive(entity), _.him(entity))
+        return ("%s backpack is squashing %s!"):format(_.possessive(entity), _.him(entity))
     end,
     Indicator = {
         None = "",
