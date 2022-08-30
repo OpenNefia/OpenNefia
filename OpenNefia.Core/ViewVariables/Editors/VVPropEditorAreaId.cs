d2b80628950bd3ccf536464cbb5ea45b0a7aad07
Ruin0x11 <ipickering2@gmail.com>
2022-08-30 00:20:17 -0700

    Map/area ID VV editors

---
+++ OpenNefia.Core/ViewVariables/Editors/VVPropEditorAreaId.cs
@@ -0,0 +1,54 @@
+﻿using OpenNefia.Core.Areas;
+using OpenNefia.Core.GameObjects;
+using OpenNefia.Core.IoC;
+using OpenNefia.Core.Maps;
+using OpenNefia.Core.Maths;
+using OpenNefia.Core.UI.Wisp;
+using OpenNefia.Core.UI.Wisp.Controls;
+using System;
+using System.Globalization;
+using static OpenNefia.Core.UI.Wisp.Controls.BoxContainer;
+
+namespace OpenNefia.Core.ViewVariables.Editors
+{
+    public sealed class VVPropEditorAreaId : VVPropEditor
+    {
+        protected override WispControl MakeUI(object? value)
+        {
+            var hBox = new BoxContainer
+            {
+                Orientation = LayoutOrientation.Horizontal,
+                MinSize = new Vector2(200, 0)
+            };
+
+            var areaId = (AreaId)value!;
+            var lineEdit = new LineEdit
+            {
+                Text = areaId.ToString(),
+                Editable = !ReadOnly,
+                HorizontalExpand = true,
+            };
+            if (!ReadOnly)
+            {
+                lineEdit.OnTextEntered += e =>
+                    ValueChanged(new AreaId(int.Parse(e.Text)));
+            }
+
+            var vvButton = new Button()
+            {
+                Text = "View Area",
+            };
+
+            vvButton.OnPressed += e =>
+            {
+                var areaMan = IoCManager.Resolve<IAreaManager>();
+                if (areaMan.TryGetArea(areaId, out var area))
+                    IoCManager.Resolve<IViewVariablesManager>().OpenVV(area);
+            };
+
+            hBox.AddChild(lineEdit);
+            hBox.AddChild(vvButton);
+            return hBox;
+        }
+    }
+}
---
+++ OpenNefia.Core/ViewVariables/Editors/VVPropEditorMapId.cs
@@ -0,0 +1,54 @@
+﻿using OpenNefia.Core.Areas;
+using OpenNefia.Core.GameObjects;
+using OpenNefia.Core.IoC;
+using OpenNefia.Core.Maps;
+using OpenNefia.Core.Maths;
+using OpenNefia.Core.UI.Wisp;
+using OpenNefia.Core.UI.Wisp.Controls;
+using System;
+using System.Globalization;
+using static OpenNefia.Core.UI.Wisp.Controls.BoxContainer;
+
+namespace OpenNefia.Core.ViewVariables.Editors
+{
+    public sealed class VVPropEditorMapId : VVPropEditor
+    {
+        protected override WispControl MakeUI(object? value)
+        {
+            var hBox = new BoxContainer
+            {
+                Orientation = LayoutOrientation.Horizontal,
+                MinSize = new Vector2(200, 0)
+            };
+
+            var mapId = (MapId)value!;
+            var lineEdit = new LineEdit
+            {
+                Text = mapId.ToString(),
+                Editable = !ReadOnly,
+                HorizontalExpand = true,
+            };
+            if (!ReadOnly)
+            {
+                lineEdit.OnTextEntered += e =>
+                    ValueChanged(new MapId(int.Parse(e.Text)));
+            }
+
+            var vvButton = new Button()
+            {
+                Text = "View Map",
+            };
+
+            vvButton.OnPressed += e =>
+            {
+                var mapMan = IoCManager.Resolve<IMapManager>();
+                if (mapMan.TryGetMap(mapId, out var map))
+                    IoCManager.Resolve<IViewVariablesManager>().OpenVV(map);
+            };
+
+            hBox.AddChild(lineEdit);
+            hBox.AddChild(vvButton);
+            return hBox;
+        }
+    }
+}
