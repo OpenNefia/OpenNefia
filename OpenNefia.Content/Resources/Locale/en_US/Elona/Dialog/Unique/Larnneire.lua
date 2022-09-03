4a5e13d8e7cf355e36a80ab6c54141ebdb68b58b
Ruin0x11 <ipickering2@gmail.com>
2022-09-03 02:05:27 -0700

    Add Larnneire's dialog

---
+++ OpenNefia.Content/Resources/Locale/en_US/Elona/Dialog/Unique/Larnneire.lua
@@ -0,0 +1,32 @@
+Elona.Dialog.Unique.Larnneire = {
+    Start = {
+        Text = "I'm glad you got well again. I thought you were a goner until just a few minutes ago.",
+        Choices = {
+            Mission = "What's your mission?",
+            NorthTyris = "Tell me about North Tyris.",
+            FairyTale = "You mentioned a fairy tale.",
+        },
+    },
+
+    Mission = {
+        Text = "We are messengers from Vindale. Our journey ends here in North Tyris at the great city of Palmia where we're supposed to have a talk with King Xabi, a man of fairness and compassion.",
+    },
+
+    NorthTyris = {
+        Text = {
+            "North Tyris is a continent in the western hemisphere of Irva. Lots of merchants and travelers set foot on North Tyris' soil with the dream of making a fortune in the ancient ruins of Nefia.",
+            "If you haven't set your destination yet, you might want to visit Vernis. It's a mining town located south of here.",
+        },
+    },
+
+    FairyTale = {
+        Text = {
+            "You may have heard of this story before...",
+            "There was once a prince who had his shape changed into that of an ugly beast by a witch. Despairing of his bloodcurdling appearance, he hid himself in a forest far from his kingdom.",
+            "One day a woman rushed into his hut and passed out, having been badly wounded by a pack of wolves. He treated her with the knowledge and fear that his ugly body might scare her. This fear turned into reality soon enough for she cried out as she woke up.",
+            "However, as days passed by and she regained her health, the woman had come to recognize the kindness and generosity that was within the heart of this beast. She decided to live with him.",
+            "They surmounted various difficulties throughout their lives and the story comes to an end when the curse set upon the prince finally breaks and the beast returned to his former shape, that of a flawlessly handsome prince. The two of them got along very well from that time on. A happy ending... although just an ordinary story, don't you agree?",
+            "The elders of Vindale used to tell me this kind of story. But I never liked this one. Perhaps it is because I felt like the beast that she loved despite the fact that his ugliness had been left behind somewhere.",
+        },
+    },
+}
