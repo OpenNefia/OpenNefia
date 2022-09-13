82589aa068b68d9780f179cd2f0e1043ad8aec9f
Ruin0x11 <ipickering2@gmail.com>
2022-09-12 22:30:38 -0700

    Unimplemented spells should have null effect for now

--- OpenNefia.Content/Resources/Locale/en_US/Elona/Party.lua
+++ OpenNefia.Content/Resources/Locale/en_US/Elona/Party.lua
@@ -2,7 +2,7 @@ Elona.Party = {
     Recruit = {
         PartyFull = "Your party is already full. You can't invite someone anymore.",
         Success = function(entity)
-            return ("%s join%s your party!"):format(_.basename(entity), _.s(entity))
+            return ("%s join%s your party!"):format(_.name(entity), _.s(entity))
         end,
     },
 }
--- OpenNefia.Content/Resources/Locale/ja_JP/Elona/Party.lua
+++ OpenNefia.Content/Resources/Locale/ja_JP/Elona/Party.lua
@@ -2,7 +2,7 @@ Elona.Party = {
     Recruit = {
         PartyFull = "仲間の最大数に達しているため、仲間にできなかった…",
         Success = function(entity)
-            return ("%sが仲間に加わった！"):format(_.basename(entity))
+            return ("%sが仲間に加わった！"):format(_.name(entity))
         end,
     },
 }
